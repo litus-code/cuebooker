53ee4d3d04ba308f4987a61060a7ebdebb3367f0
litus-code <litulandio@gmail.com>
2026-09-16 15:09:55 +0200

    Use normalized Cuebooker logo asset pack

--- app/components/CueBrand.vue
+++ app/components/CueBrand.vue
@@ -12,10 +12,10 @@ const alt = computed(() => props.decorative ? '' : 'Cuebooker')
 
 const src = computed(() => {
   if (props.variant === 'icon') {
-    return theme.value === 'light' ? '/cuebooker-icon-light-approved.png' : '/cuebooker-icon-dark.png'
+    return theme.value === 'light' ? '/logo-light.png' : '/logo-dark.png'
   }
 
-  return theme.value === 'light' ? '/cuebooker-header-light-approved.png' : '/cuebooker-header-dark-approved.png'
+  return theme.value === 'light' ? '/logo-full-light.png' : '/logo-full-dark.png'
 })
 </script>
 
