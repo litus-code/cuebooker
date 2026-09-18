fd2ed94f559e35239f8c203fa2cb27d4ed9d40c4
litus-code <litulandio@gmail.com>
2026-09-19 00:07:53 +0200

    Use QR generator compatible SVG API

--- app/services/bookingQr.ts
+++ app/services/bookingQr.ts
@@ -4,9 +4,5 @@ export function createBookingQrSvg(value: string) {
   const qr = qrcode(0, 'M')
   qr.addData(value)
   qr.make()
-  return qr.createSvgTag({
-    cellSize: 5,
-    margin: 4,
-    scalable: true
-  })
+  return qr.createSvgTag(5, 4)
 }
